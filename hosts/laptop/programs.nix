0dfbb69cd05b3d2d908eac2cca1967d333da33b7
LibreKosei <cryptokell@tutanota.com>
2026-06-23 17:04:18 +0800

    declare individual program
    Enabled programs as less as possible

---
+++ hosts/laptop/programs.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, inputs, ...}:
+{
+    programs = { 
+        obs-studio = {
+            enable = true;
+            enableVirtualCamera = true;
+            plugins = with pkgs.obs-studio-plugins; [
+                wlrobs
+                obs-pipewire-audio-capture
+                obs-gstreamer
+            ];
+        };
+
+        zsh = {
+            enable = true;
+        };
+
+        nh = {
+            enable = true;
+            clean.enable = true;
+            clean.extraArgs = "--keep-since 4d --keep 3";
+            flake = "/home/kosei/nixos";
+        };
+        
+        # Compositor
+        hyprland = {
+            enable = true;
+            package = inputs.hyprland.package.${pkgs.stdenv.hostPlatform.system}.hyprland;
+            portalPackage = inputs.hyprland.package.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+            xwayland.enable = true;
+            withUWSM = true;
+        };
+
+        tmux = {
+            enable = true;
+            baseIndex = 1;
+            keyMode = "vi";
+            shortcut = "a";
+        };
+    };
+}
